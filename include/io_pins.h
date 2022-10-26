f83622606027e4c22c237907100f78ca15a631e0
anusornint <42103118+anusornint@users.noreply.github.com>
2022-10-27 03:16:18 +0700

    Update io_pins.h
    change arduino pin

--- include/io_pins.h
+++ include/io_pins.h
@@ -3,7 +3,7 @@
 
 // PIN definition for module
 #define PIN_LMIC_NSS  10
-#define PIN_LMIC_RST  A0
-#define PIN_LMIC_DIO0 5
-#define PIN_LMIC_DIO1 6
+#define PIN_LMIC_RST  6
+#define PIN_LMIC_DIO0 4
+#define PIN_LMIC_DIO1 5
 #define PIN_LMIC_DIO2 7
